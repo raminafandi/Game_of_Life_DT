fe0b36dbe82d95af214850105aa39d6ab45a6c48
Rasim <rasimmammadoff1@gmail.com>
2020-05-03 00:55:47 +0400

    Project splitted and cmake implamented

---
+++ build/CMakeFiles/Game_Of_Life_DT.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Game_Of_Life_DT.dir/main.c.o"
+  "Game_Of_Life_DT.pdb"
+  "Game_Of_Life_DT"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Game_Of_Life_DT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
