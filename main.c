84363ae6b4227975061461bc99b475e2a6195239
Rasim <rasimmammadoff1@gmail.com>
2020-05-11 18:16:59 +0400

    SDL2 library implemented

--- main.c
+++ main.c
@@ -2,9 +2,22 @@
 #include <stdlib.h>
 #include "gameplace.h"
 #include "console.h"
+#include "sdl2.h"
 
 int main(int argc, char const *argv[])
 {
-   	printInConsole();
+    int choice;
+    printf("Do you want to display in Console => 1 or SDL2 => 2?\n");
+    scanf("%d",&choice);
+
+
+    if(choice == 1)
+   	    printInConsole();
+    else{
+        printInSDL();
+    }
+
+
+
     return 0;
 }
---
+++ sdl2/sdl2.h
@@ -0,0 +1,13 @@
+#ifndef SDL
+#define SDL
+#include <SDL2/SDL.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include <stdbool.h>
+#include"cell.h"
+#include"gameplace.h"
+
+void printInSDL();
+void draw(SDL_Renderer* renderer);
+
+#endif
