570575ef07a1acb09fbcbe81bd0c7c8ea3701c53
Ramin Afandiyev <raminefendi@gmail.com>
2020-05-13 01:09:04 +0400

    Continuous integration added to project.

---
+++ build/CMakeFiles/unit_testing.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unit_testing.dir/unit_testing.c.o"
+  "unit_testing"
+  "unit_testing.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/unit_testing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/clipped/CMakeFiles/clipped.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clipped.dir/clipped.c.o"
+  "libclipped.a"
+  "libclipped.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clipped.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
